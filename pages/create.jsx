7d9bcbd66889063a7f8d88e06e8609c43e195e33
Admin <support@unlimitpotential.com>
2023-07-24 18:39:43 -0400

    a

--- pages/create.jsx
+++ pages/create.jsx
@@ -1,7 +1,5 @@
-// pages/SamplePage.js
-
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Toaster, toast } from 'sonner';
 
 const authorizationKey = process.env.NOW_PUBLIC_API_KEY || '22-22-22';
 const UserId = process.env.NOW_PUBLIC_USER_ID || 'user_2LSoovL0oXdM3kxYgjRnhDOuFrA';
@@ -11,10 +9,20 @@ console.log(UserId);
 console.log(SpaceId);
 console.log(BlueprintId);
 
+// Helper function to show a toast notification with a delay
+const showToastWithDelay = (content, delay) => {
+  setTimeout(() => {
+    toast(content, {
+      position: 'top-right',
+      duration: 30000, // Duration for showing the toast (in milliseconds)
+    });
+  }, delay);
+};
 
 const SamplePage = () => {
   const [response, setResponse] = useState(null);
   const [nameOnCard, setNameOnCard] = useState('');
+
   const handleClick = async () => {
     const apiEndpoint = "https://nestjs-nextjs-trpc-monorepo-production.up.railway.app/actions";
 
@@ -47,19 +55,28 @@ const SamplePage = () => {
     }
   };
 
+  useEffect(() => {
+    if (response && response.webhookResponseData && response.webhookResponseData.length > 0) {
+      // Show each item's content in the webhookResponseData as a toast with a 10-second delay between each
+      response.webhookResponseData.forEach((item, index) => {
+        const delay = index * 10000; // 10 seconds delay for each item
+        showToastWithDelay(item.content, delay);
+      });
+    }
+  }, [response]); // Run this effect whenever the 'response' state changes
+
   return (
     <div>
+                <Toaster />
+
       <h1>Create Page</h1>
       <input
-                      type="text"
-                      id="name"
-
-                      onChange={(e) =>
-                        setNameOnCard(e.currentTarget.value)
-                    }     
-                      name="name-on-card"
-                      className="mt-2 block w-full rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 font-semibold text-heading placeholder:text-text/50 focus:border-primary focus:outline-none focus:ring-0 sm:text-sm"
-                      />
+        type="text"
+        id="name"
+        onChange={(e) => setNameOnCard(e.currentTarget.value)}
+        name="name-on-card"
+        className="mt-2 block w-full rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 font-semibold text-heading placeholder:text-text/50 focus:border-primary focus:outline-none focus:ring-0 sm:text-sm"
+      />
       <button onClick={handleClick}>Make API Request</button>
       {response && (
         <div>
