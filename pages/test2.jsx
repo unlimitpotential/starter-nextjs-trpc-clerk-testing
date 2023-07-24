0e00a1eeb8178c56d57da348edb3f886b9f726ba
Admin <support@unlimitpotential.com>
2023-07-24 05:32:55 -0400

    s

--- pages/test2.jsx
+++ pages/test2.jsx
@@ -78,7 +78,23 @@ export default function Home({ data }) {
       });
     }
   }, [fetchedData]);
-
+  
+  const [githubSites, setGithubSites] = useState([]);
+  const { userId} = useAuth();
+  
+  useEffect(() => {
+    const fetchGithubSites = async () => {
+      try {
+        const response = await fetch(`/api/uuid-1?userId=${UserId}`);
+        const data = await response.json();
+        setGithubSites(data);
+      } catch (error) {
+        console.error("Failed to fetch githubSites:", error);
+      }
+    };
+  
+    fetchGithubSites();
+  }, []);
   return (
     <>
       <Head>
@@ -106,7 +122,33 @@ export default function Home({ data }) {
             </button>
             <pre>{JSON.stringify(data, null, 2)}</pre>
 
-          
+            {githubSites.length > 0 ? (
+            githubSites.map((site) => (
+              <tr key={site.id}>
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  <time>{site.createdAt}</time>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {site.siteName}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {site.customCss}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                  <a
+                    href={site.subdomain}
+                    className="text-orange-600 hover:text-orange-900"
+                  >
+                    View receipt
+                  </a>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>Loading...</td>
+            </tr>
+          )}
 
           </>
         ) : (
