d5c3d48d803a94ac29b6ca493bcd6d48d46cf67a
Admin <support@unlimitpotential.com>
2023-07-24 19:18:39 -0400

    s

--- pages/create.jsx
+++ pages/create.jsx
@@ -1,11 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Toaster, toast } from 'sonner';
-import { SignedIn, SignedOut, SignOutButton, useAuth } from "@clerk/nextjs";
-import { RouterOutputs, trpc } from "../utils/trpc";
-import { AddTodo } from "../components/AddTodo/AddTodo";
-import { TodoItem } from "../components/TodoItem/TodoItem";
-import { Card } from "../components/Card/Card";
-import { Welcome } from "../components/Welcome/Welcome";
 
 const authorizationKey = process.env.NOW_PUBLIC_API_KEY || '22-22-22';
 const UserId = process.env.NOW_PUBLIC_USER_ID || 'user_2LSoovL0oXdM3kxYgjRnhDOuFrA';
@@ -65,92 +59,15 @@ const SamplePage = () => {
     if (response && response.webhookResponseData && response.webhookResponseData.length > 0) {
       // Show each item's content in the webhookResponseData as a toast with a 10-second delay between each
       response.webhookResponseData.forEach((item, index) => {
-        const delay = index * 7000; // 7 seconds delay for each item
+        const delay = index * 10000; // 10 seconds delay for each item
         showToastWithDelay(item.content, delay);
       });
     }
   }, [response]); // Run this effect whenever the 'response' state changes
 
-  const utils = trpc.useContext();
-  const { isSignedIn } = useAuth();
-
-  const { data: todos } = trpc.todo.list.useQuery(undefined, {
-    enabled: isSignedIn,
-  });
-
-  const { mutate: addTodo } = trpc.todo.add.useMutation({
-    onMutate: async (newTodo) => {
-      await utils.todo.list.cancel();
-
-      const previousTodos = utils.todo.list.getData();
-
-      const optimisticTodo = {
-        title: newTodo.title,
-        createdAt: new Date().toISOString(),
-        userId: "me",
-        id: "optimistic_" + self.crypto.randomUUID(),
-        isCompleted: false,
-      };
-      utils.todo.list.setData(undefined, (old) =>
-        old ? [...old, optimisticTodo] : [optimisticTodo]
-      );
-
-      return { previousTodos };
-    },
-    onError: (_err, _newTodo, context) => {
-      utils.todo.list.setData(undefined, context?.previousTodos);
-    },
-    onSettled: () => {
-      utils.todo.list.invalidate();
-    },
-  });
-
-  const { mutate: deleteTodo } = trpc.todo.delete.useMutation({
-    onMutate: async (deletedTodo) => {
-      await utils.todo.list.cancel();
-
-      const previousTodos = utils.todo.list.getData();
-
-      utils.todo.list.setData(undefined, (old) =>
-        old ? old.filter((i) => i.id !== deletedTodo.id) : []
-      );
-
-      return { previousTodos };
-    },
-    onError: (_err, _newTodo, context) => {
-      utils.todo.list.setData(undefined, context?.previousTodos);
-    },
-    onSettled: () => {
-      utils.todo.list.invalidate();
-    },
-  });
-
-  const { mutate: updateTodo } = trpc.todo.update.useMutation({
-    onMutate: async (updatedTodo) => {
-      await utils.todo.list.cancel();
-
-      const previousTodos = await utils.todo.list.getData();
-      utils.todo.list.setData(undefined, (old) =>
-        old
-          ? old.map((i) =>
-              i.id === updatedTodo.id ? { ...i, ...updatedTodo } : i
-            )
-          : []
-      );
-
-      return { previousTodos };
-    },
-    onError: (_err, _newTodo, context) => {
-      utils.todo.list.setData(undefined, context?.previousTodos);
-    },
-    onSettled: () => {
-      utils.todo.list.invalidate();
-    },
-  });
-
   return (
     <div>
-<Toaster expand={true} />
+                <Toaster />
 
       <h1>Create Page</h1>
       <input
@@ -161,11 +78,6 @@ const SamplePage = () => {
         className="mt-2 block w-full rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 font-semibold text-heading placeholder:text-text/50 focus:border-primary focus:outline-none focus:ring-0 sm:text-sm"
       />
       <button onClick={handleClick}>Make API Request</button>
-      {response.webhookResponseData && response.webhookResponseData.content2 && (
-            <SignOutButton />
-          )}
-
-
       {response && (
         <div>
           <h2>Response:</h2>
