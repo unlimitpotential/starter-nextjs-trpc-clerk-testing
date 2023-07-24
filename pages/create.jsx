281ad731059a5a8940e0c64b7e403efb06138e56
Admin <support@unlimitpotential.com>
2023-07-24 18:08:11 -0400

    a

--- pages/create.jsx
+++ pages/create.jsx
@@ -2,22 +2,31 @@
 
 import React, { useState } from 'react';
 
+
+const authorizationKey = process.env.NOW_PUBLIC_API_KEY || '22-22-22';
+const UserId = process.env.NOW_PUBLIC_USER_ID || '';
+const SpaceId = process.env.NOW_SPACE_ID || '';
+const BlueprintId = process.env.NOW_BLUEPRINT_ID || '';
+console.log(UserId);
+console.log(SpaceId);
+console.log(BlueprintId);
+
+
 const SamplePage = () => {
   const [response, setResponse] = useState(null);
   
   const handleClick = async () => {
     const apiEndpoint = "https://nestjs-nextjs-trpc-monorepo-production.up.railway.app/actions";
-    const token = "22-22-22";
 
     // Payload data
     const payload = {
-      field1: "age",
-      field2: "id"
+      field1: UserId,
+      field2: SpaceId
     };
 
     // Headers
     const headers = {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${authorizationKey}`,
       'Content-Type': 'application/json'
     };
 
