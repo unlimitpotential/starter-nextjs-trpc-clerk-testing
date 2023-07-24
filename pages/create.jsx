1245678e8be69f5e5502c60c65d10292ba727726
Admin <support@unlimitpotential.com>
2023-07-24 18:25:53 -0400

    f

--- pages/create.jsx
+++ pages/create.jsx
@@ -4,9 +4,9 @@ import React, { useState } from 'react';
 
 
 const authorizationKey = process.env.NOW_PUBLIC_API_KEY || '22-22-22';
-const UserId = process.env.NOW_PUBLIC_USER_ID || '';
-const SpaceId = process.env.NOW_SPACE_ID || '';
-const BlueprintId = process.env.NOW_BLUEPRINT_ID || '';
+const UserId = process.env.NOW_PUBLIC_USER_ID;
+const SpaceId = process.env.NOW_SPACE_ID;
+const BlueprintId = process.env.NOW_BLUEPRINT_ID;
 console.log(UserId);
 console.log(SpaceId);
 console.log(BlueprintId);
