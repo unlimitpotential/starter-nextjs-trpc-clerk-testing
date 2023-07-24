c74fb74dc347d962f6db32a94a23744f06e98e42
Admin <support@unlimitpotential.com>
2023-07-24 18:16:18 -0400

    e

--- pages/create.jsx
+++ pages/create.jsx
@@ -9,7 +9,7 @@ const SamplePage = () => {
   const [response, setResponse] = useState(null);
   const [nameOnCard, setNameOnCard] = useState('');
 
-  const authorizationKey = process.env.NOW_PUBLIC_API_KEY || '22-22-22';
+  const authorizationKey = process.env.NOW_PUBLIC_API_KEY || '';
 const UserId = process.env.NOW_PUBLIC_USER_ID || '';
 const SpaceId = process.env.NOW_SPACE_ID || '';
 const BlueprintId = process.env.NOW_BLUEPRINT_ID || '';
