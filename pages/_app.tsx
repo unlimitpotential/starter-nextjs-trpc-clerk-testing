bc09b600e61d427cb2a60b143239749dc1c396b8
Admin <support@unlimitpotential.com>
2023-07-26 13:17:14 -0400

    s

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { trpc } from "../utils/trpc";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from '@descope/react-sdk'
+import { SessionProvider } from "next-auth/react";
 
 import { Roboto } from "@next/font/google";
 const projectId = process.env.NEXT_PUBLIC_DESCOPE_PROJECT_ID
@@ -14,7 +15,7 @@ const roboto = Roboto({
 
 import "./styles.css";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
       <style jsx global>{`
@@ -22,10 +23,11 @@ function App({ Component, pageProps }: AppProps) {
           font-family: ${roboto.style.fontFamily};
         }
       `}</style>
-      <ClerkProvider {...pageProps}>          <AuthProvider projectId={projectId || 'DEFAULT_PROJECT_ID'}>
+      <ClerkProvider {...pageProps}>         <SessionProvider session={session}>
+     <AuthProvider projectId={projectId || 'DEFAULT_PROJECT_ID'}>
 <div>        <Component {...pageProps} /> <ReactQueryDevtools initialIsOpen={false} />  </div>
 
-         </AuthProvider>  
+         </AuthProvider>   </SessionProvider>
       </ClerkProvider>
     </>
   );
