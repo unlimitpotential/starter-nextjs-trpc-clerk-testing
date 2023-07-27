6ed28db28cf66b1cd9341f8eab1b00e472c0defe
Admin <support@unlimitpotential.com>
2023-07-26 21:05:17 -0400

    s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "picsum.photos",
+      "assets.ajio.com",
+      "rukminim2.flixcart.com",
+      "m.media-amazon.com",
+    ],
+  },
+  webpack: (config, { nextRuntime }) => {
+    // Undocumented property of next 12.
+    if (nextRuntime !== "nodejs") return config;
+    return {
+      ...config,
+      entry() {
+        return config.entry().then((entry) => ({
+          ...entry,
+        }));
+      },
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
