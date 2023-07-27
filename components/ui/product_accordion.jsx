d322a7f050f170c3d1a95ac2fa2a617bf7a4bab5
Admin <support@unlimitpotential.com>
2023-07-26 20:40:57 -0400

    s

--- components/ui/product_accordion.jsx
+++ components/ui/product_accordion.jsx
@@ -9,6 +9,7 @@ import { Card, CardDescription, CardHeader, CardTitle } from './card'; // Replac
 import Loader from './loader'; // Replace with the actual path to your Loader component
 import { useProductAccordionStore } from '../../data/stores/product_accordion_store';
 
+con
 const sampleProductData = {
   status: true,
   data: [
@@ -38,12 +39,113 @@ const ProductsPage = () => {
           <AccordionItem value={`item-${index}`} key={product.id}>
             <AccordionTrigger>
               <div className="md:flex justify-between items-center w-full space-y-2 md:space-y-0">
-                {/* Rest of the product rendering code */}
+                <div className="flex justify-center gap-6">
+                  <Image
+                    src={product.imageUrl}
+                    alt="product"
+                    priority={true}
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    placeholder="blur"
+                    blurDataURL="../assets/images/product_loader.png"
+                    className="w-[90px] h-[70px] rounded"
+                  />
+                  <div>
+                    <h5 className="text-start max-w-[25ch] md:max-w-[50ch] overflow-hidden">
+                      {product.name}
+                    </h5>
+                    <div className="flex gap-3 text-[0.7em] text-secondary-foreground">
+                      <div className="flex items-start flex-col">
+                        <span>Ordered Price</span>
+                        <span>₹{product.orderedPrice}</span>
+                      </div>
+                      <div className="flex items-start flex-col">
+                        <span>Interval</span>
+                        <span>{product.interval}S</span>
+                      </div>
+                      <div className="flex items-start flex-col">
+                        <span>Store</span>
+                        <span>
+                          {Utils.capitalize({
+                            text: product.store,
+                          })}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center px-6 gap-4 ml-9 md:ml-0">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="w-auto h-auto"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      deleteProduct(product);
+                    }}
+                  >
+                    <Trash2 className="h-4 w-4" />
+                  </Button>
+                  <div
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  ></div>
+                  <div className="flex justify-center items-center gap-1">
+                    {product.status === 'RUNNING' ? (
+                      <>
+                        {statusLoaders[index] === true ? (
+                          <Loader showLoadingText={false} size={16} />
+                        ) : (
+                          <Pause
+                            className="h-4 w-4"
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              changeProductStatus({
+                                status: 'PAUSED',
+                                product,
+                                index,
+                              });
+                            }}
+                          />
+                        )}
+                        <div className="w-[5px] h-[5px] bg-success rounded-xl"></div>
+                        <span className="text-[0.7em] text-success">Running</span>
+                      </>
+                    ) : (
+                      <>
+                        {statusLoaders[index] === true ? (
+                          <Loader showLoadingText={false} size={16} />
+                        ) : (
+                          <Play
+                            className="h-4 w-4"
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              changeProductStatus({
+                                status: 'RUNNING',
+                                product,
+                                index,
+                              });
+                            }}
+                          />
+                        )}
+                        <div className="w-[5px] h-[5px] bg-warning rounded-xl"></div>
+                        <span className="text-[0.7em] text-warning">Paused</span>
+                      </>
+                    )}
+                  </div>
+                </div>
               </div>
             </AccordionTrigger>
             <AccordionContent>
               {product.snapshots?.map((snapshot) => {
-                // Rendering snapshots data
+                const date = new Intl.DateTimeFormat('en-IN', {
+                  dateStyle: 'full',
+                  timeStyle: 'short',
+                }).format(new Date());
+
+                const formattedDate = date.replace(' at', '');
                 return (
                   <Card key={snapshot.id}>
                     <CardHeader className="flex justify-between items-center flex-row">
@@ -55,7 +157,7 @@ const ProductsPage = () => {
                         ₹{snapshot.price}
                       </CardTitle>
                       <CardDescription className="max-w-[18ch] sm:max-w-none">
-                        {/* Formatted Date */}
+                        {formattedDate}
                       </CardDescription>
                     </CardHeader>
                   </Card>
@@ -68,5 +170,6 @@ const ProductsPage = () => {
     </div>
   );
 };
+
 
 export default ProductsPage;
